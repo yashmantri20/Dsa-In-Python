5e31205a20ccc554cd00499752e60ffb9aa5f1a6
yash mantri <mantriyash2025@gmail.com>
2021-04-19 16:36:48 +0530

    Day 9 Array

---
+++ Array/Array8.py
@@ -0,0 +1,32 @@
+# Minimise the maximum difference between heights [V.IMP]
+
+def getMinDiff(arr, n, k):
+ 
+    if (n == 1):
+        return 0
+ 
+    arr.sort()
+ 
+    ans = arr[n-1] - arr[0]
+ 
+    small = arr[0] + k
+    big = arr[n-1] - k
+     
+    if (small > big):
+        small, big = big, small
+ 
+    for i in range(1, n-1):
+     
+        subtract = arr[i] - k
+        add = arr[i] + k
+
+        if (subtract >= small or add <= big):
+            continue
+ 
+        if (big - subtract <= add - small):
+            small = subtract
+        else:
+            big = add
+     
+    return min(ans, big - small)
+ 
