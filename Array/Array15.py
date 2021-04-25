dfd7d4161f1f1438f565c45e68ef324d8eed1e55
yash mantri <mantriyash2025@gmail.com>
2021-04-25 10:24:50 +0530

    Day 14 and 15 Array

---
+++ Array/Array15.py
@@ -0,0 +1,29 @@
+class Solution:
+    def merge(self,a1, a2):
+        swaps, i, j, result, m, n = 0, 0, 0, [], len(a1), len(a2)
+        ra = result.append
+        while i < m and j < n:
+            if a1[i] <= a2[j]:
+                ra(a1[i])
+                i += 1
+            else:
+                ra(a2[j])
+                j += 1
+                swaps += m - i
+        result += a1[i:]
+        result += a2[j:]    
+        return swaps, result
+    
+    def msort(self,arr):
+        n = len(arr)
+        mid = n // 2
+        if n > 1:
+            left_swaps, left_result = self.msort(arr[:mid])
+            right_swaps, right_result = self.msort(arr[mid:])
+            m_swaps, result = self.merge(left_result, right_result)
+            return m_swaps + left_swaps + right_swaps, result
+        return 0, arr
+            
+    def inversionCount(self, arr, n):
+        count,ans = self.msort(arr)
+        return count
